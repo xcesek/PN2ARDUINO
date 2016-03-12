895fe2cbcd361f4c91463d0791869761b7584de9
Pavol Cesek <paolocesek@gmail.com>
2016-03-12 22:13:31 +0100

    Petri Net logic on Arduino side
    - proof of concept
    - basic support for Petri Net engine
    - implemented: places, transitions; arcs not yes

--- src/main/resources/arduino/Place.h
+++ src/main/resources/arduino/Place.h
@@ -1,18 +1,31 @@
 #ifndef Place_h
 #define Place_h
 
+#include <Servo.h> 
 #include "Arduino.h"
+#include "Helper.h"
 
 class Place
 {
   private:
+    char *id;
     int pin;
-    char pin_type;
+    FunctionType functionType;
+    int extended;
+    int tokens;
+    int capacity;
+    
+    Servo myservo;
     
   public:
-    Place(int _pin);
-    void activate();
-    void deactivate();
+    Place(char* id);
+    Place(char* id, int _pin, FunctionType _functionType);
+    void apply();
+    int getTokens();
+    void setTokens(int _tokens);
+    void addTokens(int howMany);
+    void removeTokens(int howMany);
+    void setCapacity(int _capacity);
 };
 
 #endif
--- src/main/resources/arduino/Transition.h
+++ src/main/resources/arduino/Transition.h
@@ -2,16 +2,33 @@
 #define Transition_h
 
 #include "Arduino.h"
+#include "Helper.h"
+#include "Place.h"
 
 class Transition
 {
   private:
+    char *id;
     int pin;
-    char pin_type;
+    FunctionType functionType;
+    int extended;
+    
+    Place **inPlaces;
+    int inPlacesCount;
+    Place **outPlaces;
+    int outPlacesCount;
+    int analogTreshold;
+    int analogTresholdRangeLow;
+    int analogTresholdRangeHigh;
     
   public:
-    Transition(int _pin);
+    Transition(char* id, Place **_inPlaces, int _inPlacesCount, Place **_outPlaces, int _outPlacesCount);
+    Transition(char* id, int _pin, FunctionType _functionType, Place **_inPlaces, int _inPlacesCount, Place **_outPlaces, int _outPlacesCount);
     void fire();
+    int isActive();
+    void setAnalogTreshold(int _analogTreshold);
+    void setAnalogTresholdRange(int _analogTresholdRangeLow, int _analogTresholdRangeHigh);
 };
 
 #endif
+
